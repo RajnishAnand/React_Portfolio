bcfc7b5cecda7b898e4a74f9b4ff7ac3f6f71d8e
rajnishanand <63791262+RajnishAnand@users.noreply.github.com>
2025-02-03 23:52:51 +0530

    test

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import con from "../../assets/contact.png"
-import "./contact.css"
+import "./Contact.css"
 import { useGSAP } from '@gsap/react'
 import gsap from 'gsap'
 import {ScrollTrigger}from 'gsap/all'
